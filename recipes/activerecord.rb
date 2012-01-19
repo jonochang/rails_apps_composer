2371a597adcac0a979b298631074edf88cf0daa1
Jono Chang <jonathan.chang@silverpond.com.au>
2012-01-19 11:34:31 +1100

    updated activerecord recipe with my changes for postgres

--- recipes/activerecord.rb
+++ recipes/activerecord.rb
@@ -2,18 +2,39 @@ if config['database']
   say_wizard "Configuring '#{config['database']}' database settings..."
   old_gem = gem_for_database
   @options = @options.dup.merge(:database => config['database'])
-  gsub_file 'Gemfile', "gem '#{old_gem}'", "gem '#{gem_for_database}'"
-  if config['database'] == 'mysql'
-    if recipes.include? 'rails 3.0'
-      gsub_file 'Gemfile', "gem 'mysql2'", "gem 'mysql2', '<= 0.3.0'"
-    end
+
+  # SQLite3 gem requires special treatment.
+  gem_string = case gem_for_database
+  when "sqlite3"; "gem 'sqlite3-ruby', :require => 'sqlite3'"
+  when "mysql2"; "gem 'mysql2', '~> 0.2.6'"
+  else "gem '#{gem_for_database}'"
   end
+  gsub_file 'Gemfile', Regexp.new("gem '#{old_gem}'(, '[^']*')?(, :require => '[^']*')?"), gem_string
+
   template "config/databases/#{@options[:database]}.yml", "config/database.yml.new"
   run 'mv config/database.yml.new config/database.yml'
+  puts "db: #{@options[:database]}"
+  puts "postgres_role: #{config['postgres_role']}"
+  if @options[:database] == 'postgresql'
+    role = ask_wizard("Please enter role:")
+    run "createuser -U postgres -d #{role}" unless role.blank?
+  end
 end
 
 after_bundler do
   rake "db:create:all" if config['auto_create']
+  
+  if config['populate_rake_task']
+    populate_rake = <<-RB
+require './config/environment'
+namespace :db do
+  desc "Populate the database with sample data"
+  task :populate do
+  end
+end
+RB
+    File.open("lib/tasks/populate.rake", 'w') {|f| f.write(populate_rake)}
+  end
 end
 
 __END__
@@ -37,6 +58,12 @@ config:
         - ["SQLite", sqlite3]
         - ["Frontbase", frontbase]
         - ["IBM DB", ibm_db]
+
   - auto_create:
       type: boolean
       prompt: "Automatically create database with default configuration?"
+  
+  - populate_rake_task:
+      type: boolean
+      prompt: "Add db:populate rake task?"
+
